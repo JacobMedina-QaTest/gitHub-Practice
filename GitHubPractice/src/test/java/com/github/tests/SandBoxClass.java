634503e65510d5d7e9775af445cf9524f2ddb353
Jacob.Medina <jrmQaTest@gmail.com>
2022-12-13 11:29:15 -0500

    updated pom w/ TestNG & Logger

--- GitHubPractice/src/test/java/com/github/tests/SandBoxClass.java
+++ GitHubPractice/src/test/java/com/github/tests/SandBoxClass.java
@@ -1,5 +1,22 @@
 package com.github.tests;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import static org.testng.Assert.assertEquals;
+
 public class SandBoxClass {
 
+	public static final Logger log = LogManager.getLogger(SandBoxClass.class);
+	
+	// This is a custom wait class
+	public void customWait(double inSeconds) {
+		try {
+			long seconds = (long) (inSeconds * 1000);
+			Thread.sleep(seconds);
+		} catch (Exception e) {
+			log.error("Error: ", e);
+			assertEquals(true, false);
+		}
+	}
+	
 }
