080e9e69aa347c53e5366d66d5ead0ce61dcf7c0
Jacob.Medina <jrmQaTest@gmail.com>
2022-12-13 11:22:47 -0500

    Uploading sandbox class to github

--- GitHubPractice/src/test/java/com/github/tests/GitHubPracticeTest.java
+++ GitHubPractice/src/test/java/com/github/tests/GitHubPracticeTest.java
@@ -2,4 +2,7 @@ package com.github.tests;
 
 public class GitHubPracticeTest {
 
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
 }
---
+++ GitHubPractice/src/test/java/com/github/tests/SandBoxClass.java
@@ -0,0 +1,5 @@
+package com.github.tests;
+
+public class SandBoxClass {
+
+}
